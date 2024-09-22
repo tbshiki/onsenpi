6bfbdddff00f6dfcd399a7e766d43ff23cde2586
つぶ <info@tbshiki.com>
2024-09-22 14:46:03 +0900

    Fix get_recent_report_requests

--- onsenpi/OnsenpiSPAPIClient.py
+++ onsenpi/OnsenpiSPAPIClient.py
@@ -114,14 +114,32 @@ class OnsenpiSPAPIClient:
             print(f"Catalog API Error: {e}")
             return None
 
-    def get_recent_report_requests(self):
+    def get_recent_report_requests(self, report_type="GET_MERCHANT_LISTINGS_DATA_BACK_COMPAT"):
         """
-        最近のレポートリクエストを取得
+        最近のレポートリクエストを取得し、直近のバッチIDを返す
+        デフォルトでは 出品中の商品レポート のバッチIDを取得
         """
         reports_api = Reports(credentials=self.credentials, marketplace=self.marketplace)
-        response = reports_api.get_reports()
 
-        return response.payload
+        # レポートリクエストのリストを取得
+        response = reports_api.get_reports(reportTypes=[report_type])
+
+        # レスポンスペイロードを確認
+        if response.payload and "reports" in response.payload:
+            reports_list = response.payload["reports"]
+
+            # ペイロードがリストかどうかを確認し、要素が存在するか確認
+            if isinstance(reports_list, list) and len(reports_list) > 0:
+                # 最新のバッチIDを取得
+                recent_report = reports_list[0]
+                batch_id = recent_report.get("reportId")
+                return batch_id
+            else:
+                print("payload にレポートが含まれていません。")
+                return None
+        else:
+            print("レスポンスにpayload がありません。")
+            return None
 
     def request_listing_report(self, report_type="GET_MERCHANT_LISTINGS_ALL_DATA"):
         """
